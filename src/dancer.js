2c9dc9299011bc2ae5c1ebbf63478351eb9638d3
h3nnn4n <uber.renan@gmail.com>
2018-09-22 21:30:38 -0300

    Newbiew dancing room

---
+++ src/dancer.js
@@ -0,0 +1,116 @@
+function Dancer(x, y, w, h, options_) {
+  var options = Object.assign({
+    friction: 0.3,
+    restitution: 0.6
+  }, options_);
+
+  this.n_afraid = 10;
+  this.n_likes = 25;
+
+  this.afraid_force = -0.000025;
+  this.likes_force = 0.0001;
+
+  this.afraid = [];
+  this.likes = [];
+  this.ticks = 0;
+
+  this.c = color(random() * 255, random() * 255, random() * 255);
+
+  this.body = Bodies.rectangle(x, y, w, h, options);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
+
+  this.show = function() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke(255);
+    fill(this.c);
+    rect(0, 0, this.w, this.h);
+    pop();
+  };
+
+  this.get_position_vector = function() {
+    return createVector(this.body.position.x, this.body.position.y);
+  }
+
+  this.dance = function(dancers) {
+    if (this.ticks > 0) {
+      this.ticks -= 1;
+      this.move();
+    } else {
+      this.ticks = int(500 + random(100));
+      this.update_partners(dancers);
+    }
+  };
+
+  this.move = function() {
+    for (var prop in this.likes) {
+      this.dance_towards(this.likes[prop], this.likes_force);
+    }
+
+    for (prop in this.afraid) {
+      this.dance_towards(this.afraid[prop], this.afraid_force);
+    }
+  };
+
+  this.dance_towards = function(dancer, force) {
+    var pos1 = this.get_position_vector();
+    var pos2 = dancer.get_position_vector();
+    var pos3 = pos2.sub(pos1);
+
+    pos3.setMag(force);
+
+    Body.applyForce(
+      this.body, {
+        x: pos1.x,
+        y: pos1.y
+      },
+      {
+        x: pos3.x,
+        y: pos3.y
+      }
+    );
+  }
+
+  this.update_partners = function(dancers) {
+    this.afraid = [];
+    this.likes = [];
+
+    this.used = [];
+
+    var i;
+
+    for (i = 0, len = this.n_afraid; i < len; i++) {
+      var index = 0;
+
+      int(random(1, dancers.length));
+      this.afraid.push(dancers[index]);
+    }
+
+    for (i = 0, len = this.n_likes; i < len; i++) {
+      var index = int(random(1, dancers.length));
+      this.likes.push(dancers[index]);
+    }
+  };
+}
+
+function drawArrow(base, vec, myColor) {
+  push();
+  stroke(myColor);
+  strokeWeight(3);
+  fill(myColor);
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y);
+  rotate(vec.heading());
+  var arrowSize = 7;
+  translate(vec.mag() - arrowSize, 0);
+  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  pop();
+}
---
+++ src/dancing-room.js
@@ -0,0 +1,57 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Composites = Matter.Composites,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body;
+
+var engine;
+var world;
+var dancers = [];
+var walls = [];
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  engine.world.gravity.y = 0;
+
+  set_walls();
+  spawn_dancers();
+}
+
+function set_walls() {
+  walls.push(new Dancer(width / 2, height, width, 50, {isStatic: true}));
+  walls.push(new Dancer(width / 2, 0, width, 50, {isStatic: true}));
+  walls.push(new Dancer(0, height / 2, 50, height, {isStatic: true}));
+  walls.push(new Dancer(width, height / 2, 50, height, {isStatic: true}));
+}
+
+function spawn_dancers() {
+  for (var i = 0; i < 50; i++) {
+    dancers.push(
+      new Dancer(
+        random(100, 500),
+        random(100, 500),
+        12,
+        12
+      )
+    );
+  }
+}
+
+function draw() {
+  background(0, 0, 0);
+
+  for (var p in walls) {
+    walls[p].show();
+  }
+
+  for (p in dancers) {
+    dancers[p].dance(dancers);
+    dancers[p].show();
+  }
+
+  Engine.update(engine, 1000 / 60);
+}
